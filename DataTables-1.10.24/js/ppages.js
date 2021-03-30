f6ec4a6b7cfddafb50cf42d14e616d9901f9a40d
ronaksirwani <79188211+ronaksirwani@users.noreply.github.com>
2021-03-30 21:20:41 +0530

    Delete DataTables-1.10.24 directory

--- DataTables-1.10.24/js/ppages.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $('#ptable').DataTable({
-    "pagingType": "simple_numbers" // "simple" option for 'Previous' and 'Next' buttons only
-  });
-  $('.dataTables_length').addClass('bs-select');
-});
